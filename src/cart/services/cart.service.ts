1d331e1cf2538feb4a1ea20e729f0b218044d340
Yana Bashtyk <yana_bashtyk@epam.com>
2023-07-11 00:46:53 +0400

    feat: integrate rds

--- src/cart/services/cart.service.ts
+++ src/cart/services/cart.service.ts
@@ -1,51 +1,61 @@
 import { Injectable } from '@nestjs/common';
-
 import { v4 } from 'uuid';
-
 import { Cart } from '../models';
-
+import { knex } from '../../../db/knexconfig';
 @Injectable()
 export class CartService {
-  private userCarts: Record<string, Cart> = {};
+  // private userCarts: Record<string, Cart> = {};
 
-  findByUserId(userId: string): Cart {
-    return this.userCarts[ userId ];
+  async findByUserId(userId: string) {
+    const cart = await knex('carts').where({ user_id: userId }).first();
+
+    if (cart) {
+      const items = await knex('cart_items').where({ cart_id: cart.id });
+      cart.items = items;
+    }
+    return cart;
   }
 
-  createByUserId(userId: string) {
-    const id = v4();
-    const userCart = {
-      id,
-      items: [],
-    };
+  async createByUserId(userId: string) {
+    const [newCart] = await knex('carts')
+      .insert({
+        user_id: userId,
+        status: 'OPEN',
+        created_at: new Date(),
+        updated_at: new Date(),
+      })
+      .returning('*');
 
-    this.userCarts[ userId ] = userCart;
-
-    return userCart;
+    return newCart;
   }
 
-  findOrCreateByUserId(userId: string): Cart {
-    const userCart = this.findByUserId(userId);
+  async findOrCreateByUserId(userId: string) {
+    let cart = await this.findByUserId(userId);
 
-    if (userCart) {
-      return userCart;
+    if (!cart) {
+      cart = await this.createByUserId(userId);
     }
 
-    return this.createByUserId(userId);
+    return cart;
   }
 
-  updateByUserId(userId: string, { items }: Cart): Cart {
-    const { id, ...rest } = this.findOrCreateByUserId(userId);
+  async updateByUserId(userId: string, { items }: Cart) {
+    const cart = await this.findOrCreateByUserId(userId);
 
-    const updatedCart = {
-      id,
-      ...rest,
-      items: [ ...items ],
-    }
+    await knex('cart_items').where({ cart_id: cart.id }).del();
 
-    this.userCarts[ userId ] = { ...updatedCart };
+    const newItems = items.map((item) => ({
+      cart_id: cart.id,
+      product_id: item.product.id,
+      count: item.count,
+    }));
+    await knex('cart_items').insert(newItems);
 
-    return { ...updatedCart };
+    // Update the cart's updated_at field
+    await knex('carts').where({ id: cart.id }).update({ updated_at: new Date() });
+
+    const updatedCart = await this.findByUserId(userId);
+    return updatedCart;
   }
 
   removeByUserId(userId): void {
